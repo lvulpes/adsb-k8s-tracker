c235b58ba579ffc5a06ece82ad84999ce7a3b0d5
lvulpes <l.vulpes@proton.me>
2026-08-06 22:10:23 +0200

    Initial commit of adsb-alerts without testing

---
+++ app/adsb-alerts/main.py
@@ -0,0 +1,67 @@
+import asyncio
+import asyncpg
+import json
+
+DB_DSN = os.getenv("DB_DSN", "postgresql://postgres:password@adsb-db:5432/adsb")
+SLEEP_INTERVAL = os.getenv("SLEEP_INTERVAL", 60)
+CONFIG_PATH = os.getenv("FILTER_PATH", '/adsb-alerts/filters.json')
+
+def read_filters(file_path: str) -> list[str]:
+    """Reads mounted ConfigMap from disk."""
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+async def fetch_last_aircraft(pool, max_age_seconds: int) -> list:
+    query = """
+        SELECT *
+        FROM aircraft
+        WHERE last_updated >= NOW() - ($1 || ' seconds')::interval
+    """
+    
+    async with pool.acquire() as conn:
+        return await conn.fetch(query, max_age_seconds)
+
+async def filter_aircraft(ac: dict, ac_filter: dict) -> dict:
+    for k in ac_filter.keys():
+        if k in ac.keys():
+            # We found one matching filter name
+                logging.debug(f"Found matching key {k} on aircraft {ac['hex']}")
+            if ac_filter[k] == ac[k]:
+                # We found a match on this specific filter parameter, but need to check ALL
+                logging.debug(f"Found match for {k}: {ac[k]} == {ac_filter[k]}")
+                continue
+            else:
+                # No match, stop looking
+                return {}
+    # If we reach the end it means we had a match on all filter parameters
+    return ac
+
+async def main():
+    # Setup async DB pool once
+    db_pool = await asyncpg.create_pool(dsn=DB_DSN)
+    
+    try:
+        while True:
+            # 1. Non-blocking ConfigMap read (via thread)
+            ac_filter_list = await asyncio.to_thread(read_filters, CONFIG_PATH)
+
+            # 2. Non-blocking DB query
+            new_aircraft = await fetch_last_aircraft(db_pool, SLEEP_INTERVAL)
+
+            # 3. Process results
+            for ac in new_aircraft:
+                logging.debug(f"Checking filter for ac {ac['hex']}")
+                # Check all filters for each aircraft
+                for ac_filter in ac_filter_list:
+                    matching_ac = filter_aircraft(ac, ac_filter)
+            logging.info(f"Found match for aircraft {ac['hex']}")
+            await asyncio.sleep(SLEEP_INTERVAL)
+    finally:
+        await db_pool.close()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("\nStopped.")
+
