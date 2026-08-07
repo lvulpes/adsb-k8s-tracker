91440d79ad9e2b28a97084db0663aeecc2735eb6
lvulpes <l.vulpes@proton.me>
2026-08-07 13:23:44 +0200

    feat(adsb-alerts): Implement alerting to discord

--- app/adsb-alerts/main.py
+++ app/adsb-alerts/main.py
@@ -1,8 +1,10 @@
 import asyncio
+from datetime import datetime, timezone
 import json
 import logging
 import os
 
+import aiohttp
 import asyncpg
 
 DB_DSN = os.getenv("DB_DSN", "postgresql://postgres:password@adsb-db:5432/adsb")
@@ -10,6 +12,7 @@ SLEEP_INTERVAL = os.getenv("SLEEP_INTERVAL", "60")
 CONFIG_PATH = os.getenv("FILTER_PATH", '/app/adsb-alerts/filters.json')
 DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+ALERT_PARAMS = os.getenv("ALERT_PARAMS", "hex, flight")
 
 # Configure logging
 logging.basicConfig(
@@ -24,6 +27,7 @@ def read_filters(file_path: str) -> list[str]:
         return json.load(f)
 
 async def fetch_last_aircraft(pool, max_age_seconds: str) -> list:
+    """ Fetch the latest added aircraft from the DB. """
     query = """
         SELECT *
         FROM aircraft
@@ -34,6 +38,7 @@ async def fetch_last_aircraft(pool, max_age_seconds: str) -> list:
         return await conn.fetch(query, max_age_seconds)
 
 async def filter_aircraft(ac: dict, ac_filter: dict) -> dict:
+    """ Return an aircraft is it matches an alert filter."""
     for k in ac_filter.keys():
         if k in ac.keys():
             # We found one matching filter name
@@ -48,31 +53,72 @@ async def filter_aircraft(ac: dict, ac_filter: dict) -> dict:
     # If we reach the end it means we had a match on all filter parameters
     return ac
 
-async def main():
-    # Setup async DB pool once
-    db_pool = await asyncpg.create_pool(dsn=DB_DSN)
-    
+async def dispatch_alert(session, ac: dict, webhook: str):
+    """ Send alert to discord."""
+    # Create a list of the fields to alert on
+    alert_params = [k.strip() for k in ALERT_PARAMS.split(',') if k.strip()]
+    # Copy the data we want to send in the alert from the ac dict
+    alert_data = {k: ac[k] for k in alert_params if k in ac}
+    payload = {
+        "content": f"**Aircraft Alert!**\n```json\n{json.dumps(alert_data, indent=2)}\n```"
+    }
+   # Send the payload to discord 
     try:
-        while True:
-            # 1. Non-blocking ConfigMap read (via thread)
-            ac_filters = await asyncio.to_thread(read_filters, CONFIG_PATH)
+        async with session.post(url=webhook, json=payload) as response:
+            if response.ok:
+                return True
 
-            # 2. Non-blocking DB query
-            new_aircraft = await fetch_last_aircraft(db_pool, SLEEP_INTERVAL)
+            # log failures
+            error = await response.text()
+            logging.error(f"Failed to send alert to discord with {response.status}: {error}")
+            return False
+    except aiohttp.ClientError() as e:
+        logging.error(f"Networking error sending webhook: {e}")
+        return False
 
-            # 3. Process results
-            for ac in new_aircraft:
-                logging.debug(f"Checking filter for ac {ac['hex']}")
-                # Check all filters for each aircraft
-                for f_name, f_conf in ac_filters.items():
-                    matching_ac = filter_aircraft(ac, f_conf)
-                    if matching_ac:
-                        logging.info(f"Found match for aircraft {ac['hex']} in filter {f_name}")
-                        
-            logging.info(f"Found match for aircraft {ac['hex']}")
-            await asyncio.sleep(int(SLEEP_INTERVAL))
-    finally:
-        await db_pool.close()
+async def main():
+    db_pool = await asyncpg.create_pool(dsn=DB_DSN)
+    active_alerts = {}
+
+    # 1. Create the session ONCE outside the loop
+    async with aiohttp.ClientSession() as session:
+
+        try:
+            while True:
+                # 1. Non-blocking ConfigMap read (via thread)
+                ac_filters = await asyncio.to_thread(read_filters, CONFIG_PATH)
+
+                # 2. Non-blocking DB query
+                new_aircraft = await fetch_last_aircraft(db_pool, SLEEP_INTERVAL)
+
+                # 3. Process results
+                for ac in new_aircraft:
+                    logging.debug(f"Checking filter for ac {ac['hex']}")
+                    # Check all filters for each aircraft
+                    for f_name, f_conf in ac_filters.items():
+                        matching_ac = filter_aircraft(ac, f_conf)
+                        if matching_ac:
+                            logging.info(f"Found match for aircraft {ac['hex']} in filter {f_name}")
+                            # Alert to discord if this alert is not already active
+                            if ac.get('hex') not in active_alerts:
+                                # Send alert to discord
+                                success = await dispatch_alert(session, ac, DISCORD_WEBHOOK)
+                                if success:
+                                    logging.debug(f"Successfully sent alert to discord for hex {ac['hex']}")
+                                    active_alerts[ac['hex']] = datetime.now(timezone.utc)
+                                else:
+                                    logging.warning(f"Failed to send alert for hex {ac.get('hex')}")
+                
+                # Cleanup active alerts older than 1 hour (3600 seconds)
+                now = datetime.now(timezone.utc)
+                active_alerts = {
+                    hex_code: ts for hex_code, ts in active_alerts.items() 
+                    if (now - ts).total_seconds() < 3600
+                }
+                
+                await asyncio.sleep(int(SLEEP_INTERVAL))
+        finally:
+            await db_pool.close()
 
 if __name__ == "__main__":
     try:
